d83e2d953d38837aafa6e8b796ff4094cf61d706
Juan Di Nella <juan.dinella@gmail.com>
2013-10-13 03:10:28 -0300

    Scroll papá

---
+++ js/scroll.js
@@ -0,0 +1,26 @@
+var jump=function(e)
+{
+       //prevent the "normal" behaviour which would be a "hard" jump
+       e.preventDefault();
+       //Get the target
+       var target = $(this).attr("href");
+       //perform animated scrolling
+       $('html,body').animate(
+       {
+               //get top-position of target-element and set it as scroll target
+               scrollTop: $(target).offset().top
+       //scrolldelay: 2 seconds
+       },2000,function()
+       {
+               //attach the hash (#jumptarget) to the pageurl
+               location.hash = target;
+       });
+
+}
+
+$(document).ready(function()
+{
+       $('a[href*=#]').bind("click", jump);
+       return false;
+});
+
